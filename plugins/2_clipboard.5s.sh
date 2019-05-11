f54f717382071dc39adedc61c299cdf3abf528c1
Zanets <darg20127@gmail.com>
2019-05-12 02:25:09 +0800

    [ref] review code and ffix bad smell

--- plugins/2_clipboard.5s.sh
+++ plugins/2_clipboard.5s.sh
@@ -6,8 +6,8 @@
 # <bitbar.desc>Track up to 50 clipboard history or save as snippet.</bitbar.desc>
 # <bitbar.version>1.0</bitbar.version>
 
-# Environment
-export LANG="${LANG:-en_US.UTF-8}"
+
+# SETTINGS
 
 HIST_DIR="/tmp/bitbar/2_clipboard/history"
 HIST_MIN=0
@@ -19,6 +19,9 @@ SNIP_MAX=49
 
 LENGTH=20
 
+# ENVIRONMENT
+
+export LANG="${LANG:-en_US.UTF-8}"
 mkdir -p "$SNIP_DIR" &> /dev/null
 mkdir -p "$HIST_DIR" &> /dev/null
 
@@ -26,55 +29,47 @@ notify () {
   osascript -e "display notification \"$1\" with title \"BitBar Clipboard\"" &> /dev/null
 }
 
-# Copy file content to clipboard
-if [[ $1 == "copy" ]]; then
-  if [[ -e $2 ]]; then
-    pbcopy < "$2"
-    # notify "Copied to Clipboard."
-  fi
-  exit
-fi
+# HANDLE COMMANDS
 
-# Delete all history
-if [[ $1 == "clear-hist" ]]; then
-  pbcopy < /dev/null
-  rm -f "$HIST_DIR"/item-*.pb
-  # notify "Cleared clipboard history."
-  exit
-fi
+case "$1" in
+	"copy" )
+		[[ -z "${2// }" ]] || pbcopy < "$2"
+		exit
+		;;
+	"clear-hist" )
+		pbcopy < /dev/null
+		rm -f "$HIST_DIR"/item-*.pb
+		exit
+		;;
+	"clear-snip" )
+		rm -f "$SNIP_DIR"/item-*.pb
+		exit
+		;;
+	"store-snip" )
+		# Find a number which has not been used.
+		for i in $(seq $SNIP_MIN $SNIP_MAX); do
+			file="$SNIP_DIR/item-$i.pb"
+			if [[ ! -e $file ]]; then
+				echo -n "$(pbpaste)" > "$file"
+				exit
+			fi
+		done
+		notify "Snippets are up to $SNIP_MAX. Store failed."
+		exit
+		;;
+esac
 
-# Store snippets
-if [[ $1 == "store-snip" ]]; then
-  # Find a number which has not been used.
-  for i in $(seq $SNIP_MIN $SNIP_MAX); do
-    file="$SNIP_DIR/item-$i.pb"
-    if [[ ! -e $file ]]; then
-      printf "$(pbpaste)" > "$file"
-      # notify "Snippet stored."
-      exit
-    fi
-  done
-  
-  # All number has been used.
-  notify "Snippets are up to $SNIP_MAX. Store failed."
-fi
-
-# Delete all snippets
-if [[ $1 == "clear-snip" ]]; then
-  rm -f "$SNIP_DIR"/item-*.pb
-  # notify "Cleared snippets."
-  exit
-fi
+# DISPLAY
 
 echo '📋'
 echo "---"
 
 # Get clipboard content 
-cbContent=$(pbpaste)
+cbContent="$(pbpaste)"
 
 # Show clipboard content on clipboard section
 echo "Clipboard | color=deepskyblue"
-if [[ $cbContent != "" ]]; then
+if [[ ! -z "${cbContent//}" ]]; then
 
   # @addItem
   echo ""$cbContent" | param1=store-snip length=$LENGTH terminal=false bash='$0' refresh=true"
