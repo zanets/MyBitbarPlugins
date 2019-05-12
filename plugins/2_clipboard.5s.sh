b451ce8032cd68be77f582e94652675e096b2b41
Zanets <darg20127@gmail.com>
2019-05-12 10:24:48 +0800

    [ref] use index file to avoid heavy disk io

--- plugins/2_clipboard.5s.sh
+++ plugins/2_clipboard.5s.sh
@@ -6,16 +6,13 @@
 # <bitbar.desc>Track up to 50 clipboard history or save as snippet.</bitbar.desc>
 # <bitbar.version>1.0</bitbar.version>
 
-
 # SETTINGS
 
 HIST_DIR="/tmp/bitbar/2_clipboard/history"
-HIST_MIN=0
-HIST_MAX=49
+HIST_MAX=50
 
 SNIP_DIR="$HOME/.bitbar/appdata/2_clipboard/snippets"
-SNIP_MIN=0
-SNIP_MAX=49
+SNIP_MAX=50
 
 LENGTH=20
 
@@ -24,9 +21,83 @@ LENGTH=20
 export LANG="${LANG:-en_US.UTF-8}"
 mkdir -p "$SNIP_DIR" &> /dev/null
 mkdir -p "$HIST_DIR" &> /dev/null
+[ -f "$SNIP_DIR/index" ] || echo -n 0 > "$SNIP_DIR/index"
+[ -f "$HIST_DIR/index" ] || echo -n 0 > "$HIST_DIR/index"
+
+# HELPER
 
 notify () {
-  osascript -e "display notification \"$1\" with title \"BitBar Clipboard\"" &> /dev/null
+	osascript -e "display notification \"$1\" with title \"BitBar Clipboard\"" &> /dev/null
+}
+
+# 1: location
+getIDX () {
+	IDXF="$1/index"
+	if [ -f "$IDXF" ]; then
+		echo "$(< $IDXF)"
+	else
+		notify "Cannot found index file at $1"
+		exit
+	fi
+}
+
+# 1: location, 2: content, 3: maxi, 4: circle mode
+addItem () {
+	IDX=$(getIDX $1)
+	newIDX="$((IDX + 1))"
+	MAXIDX=$3
+
+	if [ $newIDX -gt $MAXIDX ]; then
+		if [ $4 -eq 0 ]; then
+			notify "Exceed $MAXIDX items. Aborting." 
+			return
+		else
+			newIDX=1
+		fi
+	fi
+
+	echo -n "$2" > "$1/item-$newIDX.pb"
+	echo -n "$newIDX" > "$1/index"
+}
+
+# 1: location, 2: max
+showItems () {
+	# the location of first one is recorded at index file
+	IDX=$(getIDX $1)
+	MAXIDX=$2
+	for i in $(seq 1 $MAXIDX); do
+		
+		file="$1/item-$IDX.pb"
+		if [[ -e $file ]]; then
+			content="$(< $file)"
+		
+			# @addItem
+			[[ $(( i % 10 )) == 1 ]] && echo "$i - $((i+9))"
+		
+			# @addItem
+			echo "-- $i "$content" | param1=copy param2=$file length=$LENGTH terminal=false bash='$0'"
+			
+			IDX=$((IDX - 1))
+			[ $IDX -lt 1 ] && IDX=$MAXIDX  
+		else
+			return
+		fi
+	done
+}
+
+showCurrentClipboard () {
+	CANDI="$(pbpaste)"
+	[[ -z "${CANDI//}" ]] && return
+	
+	# @addItem
+	echo ""$CANDI" | param1=add-snip length=$LENGTH terminal=false bash='$0' refresh=true"
+
+	# if not same with last history, add to history
+	IDX=$(getIDX $HIST_DIR)
+	echo -n "$CANDI" | diff "$HIST_DIR/item-$IDX.pb" - &> /dev/null
+	if [[ $? != 0 ]]; then
+		addItem "$HIST_DIR" "$CANDI" "$HIST_MAX" 1  
+	fi
 }
 
 # HANDLE COMMANDS
@@ -45,16 +116,8 @@ case "$1" in
 		rm -f "$SNIP_DIR"/item-*.pb
 		exit
 		;;
-	"store-snip" )
-		# Find a number which has not been used.
-		for i in $(seq $SNIP_MIN $SNIP_MAX); do
-			file="$SNIP_DIR/item-$i.pb"
-			if [[ ! -e $file ]]; then
-				echo -n "$(pbpaste)" > "$file"
-				exit
-			fi
-		done
-		notify "Snippets are up to $SNIP_MAX. Store failed."
+	"add-snip" )
+		addItem "$SNIP_DIR" "$(pbpaste)" "$SNIP_MAX" 0
 		exit
 		;;
 esac
@@ -64,59 +127,23 @@ esac
 echo '📋'
 echo "---"
 
-# Get clipboard content 
-cbContent="$(pbpaste)"
-
-# Show clipboard content on clipboard section
+# Display Current Clipboard Content
 echo "Clipboard | color=deepskyblue"
-if [[ ! -z "${cbContent//}" ]]; then
+showCurrentClipboard
 
-  # @addItem
-  echo ""$cbContent" | param1=store-snip length=$LENGTH terminal=false bash='$0' refresh=true"
-
-  echo -n "$cbContent" | diff "$HIST_DIR/item-$HIST_MIN.pb" - &> /dev/null
-  if [[ $? != 0 ]]; then
-    # Move previous history backwards
-    for i in $(seq $HIST_MAX $HIST_MIN); do
-      file="$HIST_DIR/item-$i.pb"
-      [[ -e $file ]] && cp $file "$HIST_DIR/item-$((i+1)).pb" &> /dev/null
-    done
-  
-    # Store to minimum number
-    echo -n "$cbContent" > "$HIST_DIR/item-$HIST_MIN.pb"
-  fi
-fi
-
-# Show history section
+# Display History
 echo "---"
 echo 'History | color=deepskyblue'
-for i in $(seq $HIST_MIN $HIST_MAX); do
-  file="$HIST_DIR/item-$i.pb"
-  if [[ -e $file ]]; then
-    content="$(< $file)"
-    # @addItem
-    [[ $(( i % 10 )) == 0 ]] && echo "$((i+1)) - $((i+10))"
-    # @addItem
-    echo "-- $((i+1)) "$content" | param1=copy param2=$file length=$LENGTH terminal=false bash='$0'"
-  fi
-done
+showItems "$HIST_DIR" "$HIST_MAX"
 
-# Show snippet section
+# Display Snippets
 echo "---"
 echo 'Snippets | color=deepskyblue'
-for i in $(seq $SNIP_MIN $SNIP_MAX); do
-  file="$SNIP_DIR/item-$i.pb"
-  if [[ -e $file ]]; then
-    content="$(< $file)"
-    # @addItem
-    [[ $(( i % 10 )) == 0 ]] && echo "$((i+1)) - $((i+10))"
-    # @addItem
-    echo "-- $((i+1)) "$content" | param1=copy param2=$file length=$LENGTH terminal=false bash='$0'"
-  fi
-done
+showItems "$SNIP_DIR" "$SNIP_MAX"
 
+# Display Commands
 echo "---"
 # @addItem
-echo "Clear all history | color=red param1=clear-hist refresh=true terminal=false bash='$0'"
+echo "Clear History | color=red param1=clear-hist refresh=true terminal=false bash='$0'"
 # @addItem
-echo "Clear all snippets | color=red param1=clear-snip refresh=true terminal=false bash='$0'"
+echo "Clear Snippets | color=red param1=clear-snip refresh=true terminal=false bash='$0'"
